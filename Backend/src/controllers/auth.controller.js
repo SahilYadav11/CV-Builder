1f810591123fb008e6f9e1edb5e69e984d0291e6
SahilYadav11 <sahilyadav0768@gmail.com>
2026-07-05 12:02:53 +0530

    updated controllers

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -43,7 +43,12 @@ async function registerUserController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
 
     res.status(201).json({
         message: "user registered successfully",
@@ -84,7 +89,12 @@ async function loginController(req, res) {
         { expiresIn: "1d" }
     )
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+        maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
     res.status(200).json({
         message: "user loggedIn successfully",
         user: {
